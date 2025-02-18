96abdf276c08f725da798c6170453bade0269a46
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-02-18 23:55:08 +0300

    no message

--- src/app/admin/posts/[id]/edit/page.tsx
+++ src/app/admin/posts/[id]/edit/page.tsx
@@ -1,15 +1,5 @@
-import type { Metadata } from 'next'
 import EditPostForm from './EditPostForm'
 
-type Props = {
-  params: { id: string }
-  searchParams?: { [key: string]: string | string[] | undefined }
-}
-
-export const metadata: Metadata = {
-  title: 'Blog Yazısı Düzenle',
-}
-
-export default async function Page(props: Props) {
-  return <EditPostForm postId={props.params.id} />
+export default function Page({ params }: { params: { id: string } }) {
+  return <EditPostForm postId={params.id} />
 } 
