d1e50da154b1de3e7475c7e574c1d788a58cd0a9
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-02-19 01:12:53 +0300

    no message

--- src/app/admin/posts/[id]/edit/page.tsx
+++ src/app/admin/posts/[id]/edit/page.tsx
@@ -3,20 +3,23 @@ import EditPostForm from './EditPostForm'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
-interface PageProps {
-  params: {
-    id: string
-  }
+type PageParams = {
+  id: string
+}
+
+type Props = {
+  params: PageParams
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params
   const supabase = createServerComponentClient({ cookies })
   
   const { data: post } = await supabase
     .from('posts')
     .select('title')
-    .eq('id', params.id)
+    .eq('id', id)
     .single()
 
   return {
@@ -24,6 +27,7 @@ export async function generateMetadata({ params }: PageProps): Promise<Metadata>
   }
 }
 
-export default async function Page(props: PageProps) {
-  return <EditPostForm postId={props.params.id} />
+export default async function Page({ params }: Props) {
+  const { id } = params
+  return <EditPostForm postId={id} />
 } 
