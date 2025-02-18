5d04f0005482ec91e1d5db5022a3d41a4d7afba6
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-02-19 00:46:22 +0300

    no message

--- src/app/admin/posts/[id]/edit/page.tsx
+++ src/app/admin/posts/[id]/edit/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 
-import { Metadata } from 'next'
 import EditPostForm from './EditPostForm'
-
-export const metadata: Metadata = {
-  title: 'Blog Yazısı Düzenle',
-}
 
 export default function Page({ params }: { params: { id: string } }) {
   return <EditPostForm postId={params.id} />
